0709cd7f81296aebe41a24f51ef6d986a97d9114
ahmedmoka12 <ahmedelbashier.2@gmail.com>
2023-02-20 17:59:09 +0200

    Commit

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -14,7 +14,6 @@ export class LoginComponent implements OnInit {
   ngOnInit(): void {
   }
   submit() {
-    this.router.navigate(["dashboard"]);
-
+      this.router.navigate(["dashboard"]);
     }
   }
